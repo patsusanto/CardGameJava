b04c790ad17fbaee108c1ad05bde0b191543df96
patsusanto <susantopatrick012@gmail.com>
2025-01-29 13:03:56 +0100

    created spring boot project

---
+++ src/main/java/dev/psusanto/CardGameJava/CardGameJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.psusanto.CardGameJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGameJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/psusanto/CardGameJava/CardGameJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.psusanto.CardGameJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
